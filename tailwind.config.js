105440994cd0a1f96c16d58cef34df4be3993fb1
PeamMiiZ <peemmygg@gmail.com>
2024-09-17 00:57:16 +0700

    add card components and determine the css styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        custom: "447px",
+      },
+      width: {
+        custom: "300px",
+      },
+      height_img: {
+        custom: "150px",
+      },
+    },
   },
   plugins: [],
-}
-
+};
